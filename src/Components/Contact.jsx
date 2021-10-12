52cc50b522e0c52078786b0b145ded3f6925f23d
Franco-Aguero <aguerofranco2002@gmail.com>
2021-10-11 23:42:42 -0300

    (client): add styles in contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedinIn, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import ContactCircle from './Views/ContactCircle';
@@ -8,13 +8,21 @@ const Contact = () => {
     const urlLinkedin = "https://www.linkedin.com/in/franco-aguero", name1 = "LinkedIn", bg1 = "rgb(0 119 181 / 81%)";
     const urlEmail = "https://mail.google.com/a/?view=cm&fs=1&to=aguerofranco2002@gmail.com", name2 = "Gmail", bg2 = "rgb(233 67 52 / 74%)";
     const urlWhatsapp = "https://wa.link/9x1can", name3 = "WhatsApp", bg3 = "rgb(77 176 81 / 85%)";
+    let [width, setWidth] = useState(window.innerWidth)
+    let [changingBg, setChangingBg] = useState(false)
+    useEffect(() => {
+        const handleResize = () => setWidth(width = window.innerWidth)
+        window.addEventListener('resize', handleResize)
+        width >= 768 ? setChangingBg(changingBg = true) : setChangingBg(changingBg = false)
+    },[width])
+
     return (
         <section className={s.SectionContainer} id="Contacto">
             <h1 style={{marginBottom:"3rem"}}>Contacto</h1>
             <div className={s.ContainerCircle}>
-                <ContactCircle image={faWhatsapp} url={urlWhatsapp} name={name3} bg={bg3}/>
-                <ContactCircle image={faLinkedinIn} url={urlLinkedin} name={name1} bg={bg1}/>
-                <ContactCircle image={faEnvelope} url={urlEmail} name={name2} bg={bg2}/>    
+                <ContactCircle image={faWhatsapp} url={urlWhatsapp} name={name3} bg={!changingBg ? bg3 :"none"}/>
+                <ContactCircle image={faLinkedinIn} url={urlLinkedin} name={name1} bg={!changingBg ? bg1 :"none"}/>
+                <ContactCircle image={faEnvelope} url={urlEmail} name={name2} bg={!changingBg ? bg2 :"none"}/>    
             </div>
         </section>
     )
