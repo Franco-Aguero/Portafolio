d4f99e281bda6e602bd356317011f4813edc3349
Franco-Aguero <aguerofranco2002@gmail.com>
2022-04-27 23:31:45 -0300

    add a new project

--- src/Components/Project/informationUsed.js
+++ src/Components/Project/informationUsed.js
@@ -3,9 +3,21 @@ import NoteBlock from '../Images/BlockOfNote2.png';
 import Wallet from '../Images/wallet.png';
 import Skillify from '../Images/skillify.png';
 import LoginInterface from "../Images/loginInterface.png"
+import AutomotiveTable from "../Images/automotiveTable.png"
 const informationUsed = {
     sectionTitle: {es:"Proyectos", en:"",},
     projectList: [
+        {
+            name:"Automotive Table",
+            img:AutomotiveTable,
+            url:"https://github.com/Franco-Aguero/Automotive-Table",
+            FrontTech:"El Front-End se llevo acabo con: React | Redux | Firebase | Styled Components | Sweet Alert2.",
+            BackTech:"En el Back-End se utilizo: NodeJS | Express | Sequelize | Postgres.",
+            information:{
+                es: `Esta es una app orientada a la industria automotriz, aquí podrán crear secciones con sus correspondientes ítems y clasificarlos asignándoles una puntuación en estrellas.`,
+                en:``,
+            }
+        },
         {
             name:"Payment Systems",
             img:Skillify,
